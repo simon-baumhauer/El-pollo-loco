fe4224c2918090346ca3a26a120588002c384477
Simon Baumhauer <simon.baumhauer@hotmail.ch>
2022-02-09 13:27:41 +0400

    character inserted

--- js/models/movable-object.class.js
+++ js/models/movable-object.class.js
@@ -1,10 +1,17 @@
 class MovableObject {
     x = 120;
-    y = 400;
+    y = 100;
+    width = 150;
+    height = 250;
     img;
 
+    loadImage(path) {
+        this.img = new Image();
+        this.img.src = path;
+    }
+
     moveRight() {
-        console.log('Move right');
+
     }
 
     moveLeft() {
--- js/models/world.class.js
+++ js/models/world.class.js
@@ -6,8 +6,22 @@ class World {
         new Chicken(),
     ];
 
+    ctx;
+    canvas;
+
+    constructor(canvas) {
+        this.ctx = canvas.getContext('2d');
+        this.canvas = canvas;
+        this.draw();
+    }
 
     draw() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
 
+        let self = this;
+        requestAnimationFrame(function() {
+            self.draw();
+        });
     }
 }
