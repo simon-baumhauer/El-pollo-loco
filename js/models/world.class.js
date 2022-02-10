ebef922a1cbc2ad202a76529162168bb6af1daa6
Simon Baumhauer <simon.baumhauer@hotmail.ch>
2022-02-10 08:42:17 +0400

    enemies insterted

--- js/models/world.class.js
+++ js/models/world.class.js
@@ -18,7 +18,9 @@ class World {
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
-
+        this.enemies.forEach(enemy => {
+            this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
+        });
         let self = this;
         requestAnimationFrame(function() {
             self.draw();
