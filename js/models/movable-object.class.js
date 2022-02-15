c5248a919b16f09fecb58bc5f072269c1cc07eec
Simon Baumhauer <simon.baumhauer@hotmail.ch>
2022-02-15 09:26:19 +0400

    loadImages function added

--- js/models/movable-object.class.js
+++ js/models/movable-object.class.js
@@ -4,10 +4,19 @@ class MovableObject {
     width = 150;
     height = 250;
     img;
+    imageChache = [];
 
     loadImage(path) {
         this.img = new Image();
         this.img.src = path;
+    }
+
+    loadImages(arr) {
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageChache[path] = path;
+        });
     }
 
     moveRight() {
