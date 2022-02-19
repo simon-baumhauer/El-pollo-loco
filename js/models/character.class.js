f0101b99381ffabe6ac5b0a8605d4a2f6de59eee
Simon Baumhauer <simon.baumhauer@hotmail.ch>
2022-02-19 08:59:05 +0400

    character img mirroring added for backwards walk

--- js/models/character.class.js
+++ js/models/character.class.js
@@ -23,12 +23,14 @@ class Character extends MovableObject {
         setInterval(() => {
             if (this.world.keyboard.LEFT) {
                 this.x -= this.speed;
+                this.otherDirection = true;
             }
         }, 1000 / 60);
 
         setInterval(() => {
             if (this.world.keyboard.RIGHT) {
                 this.x += this.speed;
+                this.otherDirection = false;
             }
         }, 1000 / 60);
 
