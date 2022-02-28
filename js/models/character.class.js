8179dcc211311c0fd1a8c10354ae076333d025bd
Simon Baumhauer <simon.baumhauer@hotmail.ch>
2022-02-28 10:37:36 +0400

    walking sound added

--- js/models/character.class.js
+++ js/models/character.class.js
@@ -1,7 +1,7 @@
 class Character extends MovableObject {
     world;
     speed = 10;
-
+    walking_sound = new Audio('audio/cartoon_footsteps_walking_fast_jogging.mp3')
     IMAGES_WALKING = [
         'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-21.png',
         'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-22.png',
@@ -22,13 +22,16 @@ class Character extends MovableObject {
 
     animate() {
         setInterval(() => {
+            this.walking_sound.pause();
             if (this.world.keyboard.LEFT && this.x > 0) {
                 this.x -= this.speed;
                 this.otherDirection = true;
+                this.walking_sound.play();
             }
             if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
                 this.x += this.speed;
                 this.otherDirection = false;
+                this.walking_sound.play();
             }
             this.world.camera_x = -this.x + 100;
         }, 1000 / 60);
