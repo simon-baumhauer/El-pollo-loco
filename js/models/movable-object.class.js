e27a728ae39074eec9fdf6b76731010fff41213e
Simon Baumhauer <simon.baumhauer@hotmail.ch>
2022-03-21 10:56:20 +0400

    instanceof added

--- js/models/movable-object.class.js
+++ js/models/movable-object.class.js
@@ -35,11 +35,13 @@ class MovableObject {
     }
 
     drawFrame(ctx) {
-    ctx.beginPath();
-    ctx.lineWidth = '5';
-    ctx.strokeStyle = 'blue';
-    ctx.rect(this.x, this.y, this.width, this.height);
-    ctx.stroke();
+        if (this instanceof Character || this instanceof Chicken) {
+            ctx.beginPath();
+            ctx.lineWidth = '5';
+            ctx.strokeStyle = 'blue';
+            ctx.rect(this.x, this.y, this.width, this.height);
+            ctx.stroke();
+        } 
     }
 
     loadImages(arr) {
