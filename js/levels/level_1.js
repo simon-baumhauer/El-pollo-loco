c730248b2b4bcc050b44a821ba8513ffe55aaef8
Simon Baumhauer <simon.baumhauer@hotmail.ch>
2022-03-28 08:12:10 +0400

    drawImage() bugfix

--- js/levels/level_1.js
+++ js/levels/level_1.js
@@ -26,10 +26,14 @@ const level_1 = new Level(
         new BackgroundObject('img/5.Fondo/Capas/5.cielo_1920-1080px.png', 719 * 3, 0),
         new BackgroundObject('img/5.Fondo/Capas/3.Fondo3/2.png', 719 * 3, 0),
         new BackgroundObject('img/5.Fondo/Capas/2.Fondo2/2.png', 719 * 3, 0),
-        new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/2.png', 719 * 3, 0),
+        new BackgroundObject('img/5.Fondo/Capas/1.suelo-fondo1/2.png', 719 * 3, 0)
 
     ],
     [
+        new Coins(), 
+        new Coins(), 
+        new Coins(), 
+        new Coins(), 
         new Coins() 
     ]
 
