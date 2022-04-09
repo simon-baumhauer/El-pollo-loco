7a4971886c617f1522ec8c16fe18665cdfb7d408
Simon Baumhauer <simon.baumhauer@hotmail.ch>
2022-04-09 17:30:43 +0400

    space scroll default blocked

--- js/game.js
+++ js/game.js
@@ -59,3 +59,9 @@ window.addEventListener('keyup', (event) => {
     }
 });
 
+window.addEventListener('keydown', function(e) {
+    if(e.keyCode == 32 && e.target == document.body) {
+      e.preventDefault();
+    }
+  });
+
